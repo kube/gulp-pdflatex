bf3d734ff94c68569b84cc939eec136b8d8a8022
Chris Feijoo <christophe@feijoo.fr>
2016-12-04 00:59:53 +0100

    Remove gulp-util dependency causing type errors

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@
      ## ## ## :##
       ## ## ##*/
 
+import { join, dirname, basename, extname } from 'path'
 import through = require('through2')
 import File = require('vinyl')
-import { replaceExtension } from 'gulp-util'
 import pdflatex from 'node-pdflatex'
 
 type Options = {
@@ -18,24 +18,27 @@ type Options = {
   texInputs?: string[]
 }
 
+const replaceExtension = (path: any, ext: string) =>
+  typeof path === 'string' ?
+    join(dirname(path), basename(path, extname(path)) + ext) : path
+
 const gulpPdflatex = (options: Options): NodeJS.ReadableStream =>
   through.obj((file: File, encoding, next) => {
 
-    if (file.isStream()) {
-      // Set output file extension to PDF
-      file.extname = '.pdf'
-      file.path = replaceExtension(file.path, file.extname)
-      file.contents = pdflatex(
-        file.contents,
-        {
-          cwd: file.dirname,
-          texInputs: options.texInputs,
-          shellEscape: options.shellEscape
-        }
-      )
-    }
-
-    next(null, file)
-  })
+      if (file.isStream()) {
+        // Set output file extension to PDF
+        file.extname = '.pdf'
+        file.path = replaceExtension(file.path, file.extname)
+        file.contents = pdflatex(
+          file.contents,
+          {
+            cwd: file.dirname,
+            texInputs: options.texInputs,
+            shellEscape: options.shellEscape
+          }
+        )
+      }
+      next(null, file)
+    })
 
 export = gulpPdflatex
