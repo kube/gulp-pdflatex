d5149005cb71dded20e3b54933e36ba0161da75b
Chris Feijoo <christophe@feijoo.fr>
2016-12-04 01:24:24 +0100

    Reindent

--- src/index.ts
+++ src/index.ts
@@ -25,20 +25,20 @@ const replaceExtension = (path: any, ext: string) =>
 const gulpPdflatex = (options: Options): NodeJS.ReadableStream =>
   through.obj((file: File, encoding, next) => {
 
-      if (file.isStream()) {
-        // Set output file extension to PDF
-        file.extname = '.pdf'
-        file.path = replaceExtension(file.path, file.extname)
-        file.contents = pdflatex(
-          file.contents,
-          {
-            cwd: file.dirname,
-            texInputs: options.texInputs,
-            shellEscape: options.shellEscape
-          }
-        )
-      }
-      next(null, file)
-    })
+    if (file.isStream()) {
+      // Set output file extension to PDF
+      file.extname = '.pdf'
+      file.path = replaceExtension(file.path, file.extname)
+      file.contents = pdflatex(
+        file.contents,
+        {
+          cwd: file.dirname,
+          texInputs: options.texInputs,
+          shellEscape: options.shellEscape
+        }
+      )
+    }
+    next(null, file)
+  })
 
 export = gulpPdflatex
